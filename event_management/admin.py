bef97d2b9c2ca840042e6bd7a2bea71c40632c93
Samiul Baree Sifat <63223565+ssifu@users.noreply.github.com>
2023-11-29 00:01:59 +0600

    Basic user registration done (some modification needed in futuer)

--- event_management/admin.py
+++ event_management/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+from .models import Users, Event
+
 # Register your models here.
+admin.site.register(Users)
+admin.site.register(Event)
--- event_management/models.py
+++ event_management/models.py
@@ -1,3 +1,38 @@
+# from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+class Users(models.Model):
+    id = models.AutoField(primary_key=True, unique=True, null=False)
+    username = models.CharField(max_length=50, unique=True)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=255)
+    first_name = models.CharField(max_length=50, blank=True, null=True)
+    last_name = models.CharField(max_length=50, blank=True, null=True)
+
+    def __str__(self):
+        return self.username
+    
+class Event(models.Model):
+    id = models.AutoField(primary_key=True, unique=True, null=False)
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    date = models.DateField()
+    time = models.TimeField()
+    location_name = models.CharField(max_length=100, blank=True, null=True)
+    max_slots = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+    
+# class UserEventRegistration(models.Model):
+#     user = models.ForeignKey(User, on_delete=models.CASCADE)
+#     event = models.ForeignKey(Event, on_delete=models.CASCADE)
+#     registration_date = models.DateTimeField(auto_now_add=True)
+
+#     class Meta:
+#         unique_together = ('user', 'event')
+
+#     def __str__(self):
+#         return f"{self.user.username} - {self.event.title}"
